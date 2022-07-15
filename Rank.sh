a663abf4fe21c732752e0ab8ff25b81924580eb6
Richie Cahill <richie@tmmworkshop.com>
2022-07-14 22:10:36 -0400

    clened up Rank.sh

--- Rank.sh
+++ Rank.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+URL="tmmworkshop"
 
-tmmworkshop
-
-# sort_by(.score) sort_by(.urls[].score)
-# This can probly improved this if i can get jq to cooperate
-raw=$(curl https://archlinux.org/mirrors/status/json/ | cut -d" "  -f10-)
-data=$(echo ${raw::-15} | jq 'sort_by(.score)')
+# One liner for fun
+# curl https://archlinux.org/mirrors/status/json/ | jq '.urls|sort_by(.score)' | jq -c '.[] | select( .country_code | contains("US"))' | sed '/"score":null/d' | grep -n "$URL" | cut -f1 -d":" >> US.csv
+data=$(curl https://archlinux.org/mirrors/status/json/ | jq '.urls|sort_by(.score)')
 
 echo "$data" | jq -c '.[] | select( .country_code | contains("US"))' | sed '/"score":null/d' | grep -n "$URL" | cut -f1 -d":" >> US.csv
 echo "$data" | jq -c '.[] | select( .active | contains(true))' | sed '/"score":null/d' | grep -n "$URL" | cut -f1 -d":" >> World.csv
